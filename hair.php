768c57d01919d88864aa56824cd32766023ff385
ermuks <49936282+ermuks@users.noreply.github.com>
2020-09-02 23:53:14 +0900

    Add files via upload

---
+++ hair.php
@@ -0,0 +1,9 @@
+﻿<html>
+<head>
+    <meta charset="utf-8" />
+    <title>뭐 확씨</title>
+</head>
+<body>
+    <? "<img id=\"hairimg\" src=\"https://ermuks.gitgub.io/maple/".$_GET['img']."png\" />"; ?>
+</body>
+</html>
